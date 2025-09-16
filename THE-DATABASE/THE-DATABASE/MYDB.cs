bc753d24bd420d143c055d05f81441c8bf7adee3
MuhammadIzhaanHumayun <149602934+MuhammadIzhaanHumayun@users.noreply.github.com>
2025-09-16 15:20:42 +0500

    Add files via upload

---
+++ THE-DATABASE/THE-DATABASE/MYDB.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Library_Management_System.THE_DATABASE
+{
+    internal class MYDB
+    {
+        private MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;User Id=root;Password=;Database=lms;");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+        //create a function to return the table of genre data
+        public DataTable getData(string query, MySqlParameter[] parameters)
+        {
+            MySqlCommand command = new MySqlCommand(query, connection);
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table;
+        }
+        public int setData(string query, MySqlParameter[] parameters)
+        {
+            MySqlCommand command = new MySqlCommand(query, connection);
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            openConnection();
+            int commandState = command.ExecuteNonQuery();
+            closeConnection();
+            return commandState;
+        }
+    }
+}
